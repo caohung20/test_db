d970a191f37fbe36a7667b647a938d7041e8d597
caohung20 <131183904+caohung20@users.noreply.github.com>
2023-08-21 13:39:05 +0700

    after clean

--- layer_input.py
+++ layer_input.py
@@ -2,6 +2,7 @@ import streamlit as st
 import streamlit_authenticator as stauth
 from connector import Database
 from testcode import Orders
+import pandas as pd
 
 class LayerInput(Database):
     def __init__(self):
@@ -11,15 +12,8 @@ class LayerInput(Database):
             self.ord_name[i] = str(self.ord_name[i]).replace('(','')
             self.ord_name[i] = str(self.ord_name[i]).replace(')','')
             self.ord_name[i] = str(self.ord_name[i]).replace(',','')
-        st.write(self.ord_name)
         option = st.selectbox('Information of:',options=self.ord_name)
-        st.write(option)
-        new_option = str(option)
-        new_option.replace(",","")
-        new_option.replace('(','')
-        new_option.replace(')','')
-        st.write(new_option)
-        row = self.ord.query_row(new_option)
+        row = self.ord.query_row(option)
         st.write(row)
 
 if __name__ == '__main__':
